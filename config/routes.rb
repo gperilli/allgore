bbfcbed9b133675083178d52d8de7d5460ad8c49
Gareth Perilli <garethperilli@gmail.com>
2021-12-22 13:15:11 +0900

    routes and view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'bookmarks/controller'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'movies#index'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
+  resources :movies, only: [:index, :show]
 end
