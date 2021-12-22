8552051bbd21c6c4dc3b4f76e7b9ab45b6d105f6
Gareth Perilli <garethperilli@gmail.com>
2021-12-22 14:42:48 +0900

    clean state before local server de@ploy issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'movielistconnectors/controller'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'movies#index'
+  resources :lists, except: [:edit, :update] do
+    resources :movielistconnectors, only: [:new, :create]
+  end
+  resources :movielistconnectors, only: :destroy
+  resources :movies, only: [:index, :show]
 end
