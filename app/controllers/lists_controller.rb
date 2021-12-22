cb3998cd0220290facdefe196516dfb57f2f9ddf
Gareth Perilli <garethperilli@gmail.com>
2021-12-22 13:16:34 +0900

    controller files

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,37 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    # @review = Review.new(list: @list)
+  end
+
+  #def new
+  #  @list = List.new
+  #end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      @lists = List.all
+      render :index
+    end
+  end
+
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :photo)
+  end
+end
