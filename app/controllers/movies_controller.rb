901f33bed1904f1142ed988c556fdff84e218094
Gareth Perilli <garethperilli@gmail.com>
2021-12-23 15:05:57 +0900

    search function added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,25 @@
 class MoviesController < ApplicationController
 
   def index
-    if params[:query].present?
-      @movies = Movie.where(title: params[:query])
-    else
-      @movies = Movie.all #Movie.page params[:page]
-    end
+  @carousel_movie_titles = [] 
+  @carousel_movie_titles[0] = "Dracula" 
+  @carousel_movie_titles[1] = "Alien" 
+  @carousel_movie_titles[2] = "Saw II" 
+  @carousel_movie_titles[3] = "A Nightmare on Elm Street" 
+  @carousel_movie_titles[4] = "Psycho" 
+  @carousel_movie_titles[5] = "The Ring" 
+
+      if params[:query].present?
+        sql_query = " \
+          movies.title ILIKE :query \
+          OR movies.release_date ILIKE :query \
+          OR lists.name ILIKE :query \
+        "
+        @movies = Movie.joins(:lists).where(sql_query, query: "%#{params[:query]}%")
+      else
+        @movies = Movie.all #Movie.page params[:page]
+      end
+    
     # @lists = List.all
   end
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
 has_many :movielistconnectors
-
+has_many :lists, through: :movielistconnectors
 
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
