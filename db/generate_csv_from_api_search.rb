1df9494478a785b43e298a88187b01f9a5f3b0d8
Gareth Perilli <garethperilli@gmail.com>
2022-01-26 19:17:50 +0900

    seeding big horror movie database

--- db/generate_csv_from_api_search.rb
+++ db/generate_csv_from_api_search.rb
@@ -8,7 +8,7 @@ def generate_csv_from_api_search
   # tmdb has about 704,457 movies
   puts 'Seeding from TMDB into csv file'
   waiting_dots
-  new_movie_index = 8644
+  new_movie_index = 9965
   horror_movies_n = 1
   movie_titles_array = []
   movie_hash_for_csvarray = get_tmdb_movies(new_movie_index, horror_movies_n, movie_titles_array, 121)
